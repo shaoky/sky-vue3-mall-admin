87d487f7cacfd0f293d854861c0c85577cb06457
shaoky <634207476@qq.com>
2022-04-16 09:59:36 +0800

    chore: 增加lodash

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import styleImport from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001
+  },
   plugins: [
     vue(),
   ]

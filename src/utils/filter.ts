630d2eb2e6cfc3e7e21e7209183a33138033499f
shaoky <634207476@qq.com>
2022-08-04 11:31:51 +0800

    feat: filter增加布尔转数字类型

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -8,10 +8,19 @@ class Filter {
         }
         return text
     }
+
+    booleanToNumber (value: boolean) {
+        if (value) {
+            return 1
+        } else {
+            return 0
+        }
+    }
 }
 
 export interface filtersModel  {
     isOpen (value: boolean): string;
+    booleanToNumber(value: boolean): number;
 }
 
 export default new Filter()

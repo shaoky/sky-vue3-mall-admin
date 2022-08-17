d159a42c9e8481150cd01ccb064efb1fdefad4a8
shaoky <634207476@qq.com>
2022-08-17 20:37:48 +0800

    chore: element改成中文

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,28 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from "./router";
-// import store from './store';
-import './assets/less/main.less';
-import ElementPlus, { ElNotification } from 'element-plus';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from "./router"
+import './assets/less/main.less'
+import ElementPlus, { ElNotification } from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import filter from './utils/filter';
-import VueUeditorWrap from 'vue-ueditor-wrap';
+import VueUeditorWrap from 'vue-ueditor-wrap'
 import { createPinia } from 'pinia'
+
 const app = createApp(App)
 const pinia = createPinia()
 app.config.globalProperties.$filters = filter
 app.config.globalProperties.$ElNotification = ElNotification
-
 router.beforeEach((to, from , next) => {
     document.title = 'xxx商城'
     next()
 })
+
 app.use(router)
-.use(ElementPlus)
+.use(ElementPlus, {
+    locale: zhCn
+})
 .use(VueUeditorWrap)
 .use(pinia)
 .mount('#app')

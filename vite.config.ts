32b69cbc6968d74b1fc813cd934bc25d778e624e
shaoky <634207476@qq.com>
2022-08-19 18:46:04 +0800

    build: 增加打包分析

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vitejs.dev/config/
+const lifecycle = process.env.npm_lifecycle_event
+
 export default defineConfig({
   server: {
     port: 3001
@@ -11,7 +13,11 @@ export default defineConfig({
   plugins: [
     vue(),
     // 解决vue3下script setup语法糖下，手动设置组件name不方便的问题，可能会导致vue组件debuger时，断点位置不正确问题，使用前请慎重（直至0.4.0版本依旧有该问题）
-    vueSetupExtend()
+    vueSetupExtend(),
+    // 打包分析
+    lifecycle === 'report'
+      ? visualizer({open: true, brotliSize: true, filename: 'report.html'})
+      : null
   ],
   resolve: {
     alias: {

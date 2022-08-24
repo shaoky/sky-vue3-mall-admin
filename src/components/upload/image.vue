9136eea023fa5f9605841c6923966bb8945e6808
shaoky <634207476@qq.com>
2022-08-24 23:57:08 +0800

    feat: 增加upload-image组件

---
+++ src/components/upload/image.vue
@@ -0,0 +1,21 @@
+<template>
+    <el-upload
+        class="avatar-uploader"
+        :action="imgBaseUrl"
+        :show-file-list="false"
+        :on-success="handleAvatarSuccess">
+        <img v-if="imageUrl" :src="imageUrl" class="avatar">
+        <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+    </el-upload>
+</template>
+<script lang="ts" setup>
+import { imgBaseUrl } from '@/config/env'
+
+const emit = defineEmits(['success'])
+defineProps(['imageUrl'])
+
+const handleAvatarSuccess = (res) => {
+    emit('success', res.data.url)
+}
+
+</script>

75e4a11468f19aa87f8cf142cb7fde0e0e1867ac
shaoky <634207476@qq.com>
2022-08-24 23:50:34 +0800

    refactor: 重构文章详情

--- src/views/business/article/info.vue
+++ src/views/business/article/info.vue
@@ -1,109 +1,92 @@
 <template>
-    <div class="articleInfo">
-        <el-form label-width="120px">
-            <el-form-item label="标题：">
-                <el-input style="width:300px;" v-model="form.title"></el-input>
-            </el-form-item>
-            <el-form-item label="类型：" >
-                <el-select v-model="form.typeId" placeholder="请选择">
-                    <el-option v-for="(item,index) in typeList" :key="index" :label="item.title" :value="item.id"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="广告排序：">
-                <el-input style="width:300px;" v-model="form.sort"></el-input>
-            </el-form-item>
-            <el-form-item label="文章主图：">
-                <el-upload
-                    class="avatar-uploader"
-                    :action="imgBaseUrl"
-                    :show-file-list="false"
-                    :on-success="handleAvatarSuccess">
-                    <img v-if="form.imageUrl" :src="form.imageUrl" class="avatar">
-                    <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
-                </el-upload>
-            </el-form-item>
-            <el-form-item label="内容：" class="ueditor-box">
-                <ueditor ref="ue"  :config="{initialContent: form.content}" v-if="form.content || !id"></ueditor>
-            </el-form-item>
-            <el-form-item label="是否启用：">
-                <el-checkbox v-model="form.isOpen"></el-checkbox>
-            </el-form-item>
-            <div class="btn">
-                <el-button type="primary" @click="onSubmit">保存</el-button>
-            </div>
-        </el-form>
-    </div>
+    <el-form label-width="120px">
+        <el-form-item label="标题：">
+            <el-input style="width:300px;" v-model="form.title"></el-input>
+        </el-form-item>
+        <el-form-item label="类型：" >
+            <el-select v-model="form.typeId" placeholder="请选择">
+                <el-option v-for="(item,index) in typeList" :key="index" :label="item.title" :value="item.id"></el-option>
+            </el-select>
+        </el-form-item>
+        <el-form-item label="广告排序：">
+            <el-input style="width:300px;" v-model.number="form.sort"></el-input>
+        </el-form-item>
+        <el-form-item label="文章主图：">
+            <upload-image :imageUrl="form.imageUrl" @success="handleAvatarSuccess"></upload-image>
+        </el-form-item>
+        <el-form-item label="内容：" class="ueditor-box">
+            <Ueditor ref="ue"  :config="{initialContent: form.content}" v-if="loading"></Ueditor>
+        </el-form-item>
+        <el-form-item label="是否启用：">
+            <el-checkbox v-model="form.isOpen"></el-checkbox>
+        </el-form-item>
+        <div class="btn-submit">
+            <el-button type="primary" @click="onSubmit">保存</el-button>
+        </div>
+    </el-form>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, onMounted, toRefs, ref } from 'vue';
-import { imgBaseUrl } from '../../../config/env'
-import { getArticleTypeList, addArticle, updateArticle, getArticleInfoApi } from '../../../api/getData'
-import ueditor from '../../../components/ueditor.vue'
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { getArticleTypeList, addArticle, updateArticle, getArticleInfoApi } from '@/api/getData'
+import Ueditor from '@/components/ueditor.vue'
 import { useRoute } from 'vue-router'
-import { Models } from '@/rapper'
+import type { Models } from '@/rapper'
+import UploadImage from '@/components/upload/image.vue'
 
-export default defineComponent({
-    components: {
-        ueditor
-    },
-    setup() {
-        const ue = ref(null)
-        const state = reactive({
-            imgBaseUrl: imgBaseUrl,
-            id: 0,
-            form: {} as Models['GET/admin/article/info']['Res']['data']['info'],
-            positionList: [],
-            typeList: [] as Models['GET/admin/article/type/list']['Res']['data']['list'],
-            dialogImageUrl: '',
-            dialogVisible: false
-        })
+type TypeModel = Models['GET/admin/article/type/list']['Res']['data']['list'][0]
 
-        onMounted(async() => {
-            let params = useRoute().params
-            if (params.id) {
-                state.id = Number(params.id) 
-                await _getArticleTypeList()
-                getArticleInfo(state.id)
-            }
-        })
-
-        const getArticleInfo = async (id: number) => {
-            const data = await getArticleInfoApi({id: id})
-            state.form = data.info
-        }
-
-        const _getArticleTypeList = async () => {
-            const data = await getArticleTypeList()
-            state.typeList = data.list
-        }
-        const handleAvatarSuccess = (res, file) => {
-            state.form.imageUrl = res.data.url
-        }
-
-        const onSubmit = async () => {
-            // @ts-ignore
-            state.form.content = ue.value.content
-            if (state.id) {
-                await updateArticle(state.form)
-            } else {
-                await addArticle(state.form)
-            }
-        }
-
-        return {
-            ...toRefs(state),
-            onSubmit,
-            handleAvatarSuccess,
-            ue
-        }
-    }
+const route = useRoute()
+const ue = ref()
+let loading = ref(false)
+let form = ref({
+    id: 0,
+    title: '',
+    imageUrl: '',
+    isOpen: true,
+    sort: 10,
+    typeId: '' as string | number,
+    content: ''
 })
+let typeList = ref<TypeModel[]>([])
 
+const initData = async() => {
+    await _getArticleTypeList()
+    if (route.params.id) {
+        await getArticleInfo(+route.params.id)
+    }
+    loading.value = true
+}
+
+const getArticleInfo = async (id: number) => {
+    const data = await getArticleInfoApi({id})
+    form.value = data.info
+}
+
+const _getArticleTypeList = async () => {
+    const data = await getArticleTypeList()
+    typeList.value = data.list
+}
+
+const handleAvatarSuccess = (url: string) => {
+    form.value.imageUrl = url
+}
+
+const onSubmit = async () => {
+    form.value.content = ue.value.content
+    if (form.value.id) {
+        await updateArticle({
+            ...form.value,
+            typeId: +form.value.typeId
+        })
+    } else {
+        await addArticle({
+            ...form.value,
+            typeId: +form.value.typeId
+        })
+    }
+    window.history.back()
+}
+
+initData()
 </script>
-
-
-<style scoped lang="less">
-
-.btn{text-align: center}
-</style>

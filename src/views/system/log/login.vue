fa773d24a5d216b68a4848609691a81a07eec43c
shaoky <634207476@qq.com>
2022-08-25 23:55:21 +0800

    refactor: 重构登录日志

--- src/views/system/log/login.vue
+++ src/views/system/log/login.vue
@@ -1,60 +1,11 @@
 <template>
-    <div class="admin-add">
-        <el-table border :data="list">
-            <el-table-column label="登陆时间" prop="time"></el-table-column>
-            <el-table-column label="用户名" prop="username"></el-table-column>
-        </el-table>
-        <pagination @handleCurrentChange='handleCurrentChange' @handleSizeChange="handleSizeChange" :size="size" :total="total"></pagination>
-
-    </div>
+    <Table :api="getLogLogin">
+        <el-table-column label="登陆时间" prop="time"></el-table-column>
+        <el-table-column label="用户名" prop="username"></el-table-column>
+    </Table>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, onMounted, toRefs } from 'vue'
+<script lang="ts" setup>
+import Table from '@/components/table/index.vue'
 import { getLogLogin } from '@/api/getData'
-import Pagination from '@/components/pagination.vue'
-import { Models } from '@/rapper'
-
-export default defineComponent({
-    components: {Pagination},
-    setup() {
-        const state = reactive({
-            show: false,
-            id: '',
-            list: [] as Models['GET/admin/system/log/login']['Res']['data']['list'],
-            page: 1,
-            size: 10,
-            total: 0
-        })
-
-        onMounted(() => {
-            getData()
-        })
-
-        const getData = async() => {
-            const data = await getLogLogin({page: state.page, size: state.size})
-            state.list = data.list
-            state.total = data.count
-        }
-
-        const handleCurrentChange = (page: number) => {
-            state.page = page
-            getData()
-        }
-
-        const handleSizeChange = (size: number) => {
-            state.size = size
-            getData()
-        }
-
-        return {
-            ...toRefs(state),
-            handleCurrentChange,
-            handleSizeChange
-        }
-    }
-})
 </script>
-
-<style scoped lang="less">
-</style>

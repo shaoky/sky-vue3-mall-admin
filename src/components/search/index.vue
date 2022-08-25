3773606cfde81514b92d59a80759fc662bdb3380
shaoky <634207476@qq.com>
2022-08-25 23:32:30 +0800

    feat: 增加搜索组件-级联选择

--- src/components/search/index.vue
+++ src/components/search/index.vue
@@ -16,11 +16,21 @@
             >
                 <el-option 
                     v-for="item1 in item.items" 
-                    :key="item1.value" 
-                    :label="item1.label" 
-                    :value="item1.value"
+                    :key="item1.value"
+                    :label="item1[item.props?.label || 'label']" 
+                    :value="item1[item.props?.value || 'value']"
                 ></el-option>
             </el-select>
+            <!-- 级联选择器 -->
+            <el-cascader
+                v-model="search[item.field]"
+                :options="item.items"
+                :props="item.props"
+                placeholder="请选择"
+                @change="onCascaderSearch"
+                clearable
+                v-if="item.type === 'cascader'"
+            ></el-cascader>
         </el-form-item>
         <el-form-item>
             <el-button type="primary" @click="onSearch">查询</el-button>
@@ -50,6 +60,17 @@ watchEffect(() => {
 })
 
 const onSearch = () => {
+    emit('change', search.value)
+}
+
+/**
+ * 级联返回值处理：[1,2,3] => 3
+ */
+const onCascaderSearch = () => {
+    const filters = props.data.filter(item => item.type === 'cascader')
+    filters.forEach(item => {
+        search.value[item.field] = search.value[item.field].at(-1)
+    })
     emit('change', search.value)
 }
 
--- src/components/search/interface.ts
+++ src/components/search/interface.ts
@@ -2,10 +2,11 @@ export interface SearchModel {
     label: string // 名称
     field: string // 字段
     value: string | number // 初始值
-    type: 'text' | 'select' // 类型
+    type: 'text' | 'select' | 'cascader' // 类型
     items?: any // 数据列表，用于select
     api?: (params?: any) => Promise<any> // 简化获取数据操作
-    props?: Props // 修改接口返回的字段
+    params?: object // api参数
+    props?: Props // 设置显示字段
 }
 
 interface Props {

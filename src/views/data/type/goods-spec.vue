739dcd9d443ab41337583d51b4500fe50f169a97
shaoky <634207476@qq.com>
2022-08-25 23:51:03 +0800

    refactor: 重构产品规格

--- src/views/data/type/goods-spec.vue
+++ src/views/data/type/goods-spec.vue
@@ -1,22 +1,23 @@
 <template>
-    <el-button type="primary" @click="onEdit(bakForm)">新增</el-button>
-
-    <el-table class="mt20" border :data="list">
+    <Table :api="getGoodsSpecList" :api-params="{goodsClassId: initForm.goodsClassId}" :pagination="false" ref="tableRef">
+        <template #btns>
+            <el-button type="primary" @click="onEdit(initForm)">新增</el-button>
+        </template>
         <el-table-column label="规格名称" prop="name"></el-table-column>
         <el-table-column label="可选列表" prop="content"></el-table-column>
         <el-table-column label="排序" prop="sort" width="80px;"></el-table-column>
         <el-table-column label="状态" width="80px;">
             <template #default="scope">{{$filters.isOpen(scope.row.isOpen)}}</template>
         </el-table-column>
-        <el-table-column label="操作">
+        <el-table-column label="操作" width="150px;">
             <template #default="scope">
                 <el-button type="primary" link @click="onEdit(scope.row)">编辑</el-button>
                 <el-button type="primary" link @click="onDelete(scope.row.id)">删除</el-button>
             </template>
         </el-table-column>
-    </el-table>
+    </Table>
 
-    <el-dialog :title="form.id ? '编辑规格' : '新增规格'" v-model="dialogVisible" width="500px" center>
+    <el-dialog :title="form.id ? '编辑' : '新增'" v-model="dialogVisible" width="500px" center>
         <el-form label-width="120px">
             <el-form-item label="规格名称：">
                 <el-input style="max-width:300px;" v-model="form.name"></el-input>
@@ -34,81 +35,56 @@
         </el-form>
         <span slot="footer" class="dialog-footer">
             <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="onSubmit">确 定</el-button>
+            <el-button type="primary" @click="onSubmit">保存</el-button>
         </span>
     </el-dialog>
 </template>
-<script lang="ts">
-import { defineComponent, ref, reactive, onMounted, toRefs } from 'vue'
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Table from '@/components/table/index.vue'
 import { getGoodsSpecList, addGoodsSpec, updateGoodsSpec, deleteGoodsSpec } from '@/api/getData'
+import useMessageBox from '@/hooks/useMessageBox'
 import { useRoute } from 'vue-router'
-import { Models } from '@/rapper'
 
-export default defineComponent({
-    setup() {
-        const state = reactive({
-            dialogVisible: false,
-            list: [] as Models['GET/admin/goods/spec/list']['Res']['data']['list'],
-            form: {
-                id: undefined,
-                goodsClassId: 0,
-                name: '',
-                content: '',
-                sort: 20,
-                isOpen: false
-            },
-            bakForm: {}
-        })
-
-        onMounted (() => {
-            let params = useRoute().params
-            if (params.id) {
-                state.form.goodsClassId = Number(params.id) 
-            }
-            state.bakForm = {...state.form}
-            _getGoodsAttrList()
-        })
-
-        const _getGoodsAttrList = async() => {
-            let res = await getGoodsSpecList({goodsClassId: state.form.goodsClassId})
-            state.list = res.list
-        }
-
-        const onEdit = (item) => {
-            state.form = {...item}
-            state.dialogVisible = true
-        }
-
-        const onSubmit = async() => {
-            if (state.form.id) {
-                await updateGoodsSpec({
-                    id: state.form.id,
-                    content: state.form.content,
-                    isOpen: state.form.isOpen,
-                    name: state.form.name,
-                    sort: state.form.sort
-                })
-            } else {
-                await addGoodsSpec(state.form)
-            }
-            state.dialogVisible = false
-            _getGoodsAttrList()
-        }
-
-        const onDelete = async(id: number) => {
-            await deleteGoodsSpec({id})
-            _getGoodsAttrList()
-        }
-
-        return {
-            ...toRefs(state),
-            onEdit,
-            onSubmit,
-            onDelete
-        }
-    }
+let route = useRoute()
+let tableRef = ref()
+let initForm = ref({
+    goodsClassId: 0,
+    sort: 10,
+    type: 1
 })
+let form = ref({
+    id: 0,
+    name: '',
+    sort: 0,
+    content: '',
+    isOpen: true,
+    goodsClassId: 0
+})
+let dialogVisible = ref(false)
+
+initForm.value.goodsClassId = Number(route.params.id) 
+
+const onEdit = (row) => {
+    form.value = { ...row }
+    dialogVisible.value = true
+}
+
+const onDelete = async(id: number) => {
+    await useMessageBox()
+    await deleteGoodsSpec({id})
+    tableRef.value.refresh()
+}
+
+const onSubmit = async() => {
+    if (form.value.id) {
+        await updateGoodsSpec(form.value)
+    } else {
+        await addGoodsSpec(form.value)
+    }
+    dialogVisible.value = false
+    tableRef.value.refresh()
+}
 
 </script>
-<style lang="less" scoped>
-</style>

5c635645b51d8a8a7937209fcf4b19cb5d1056e7
shaoky <634207476@qq.com>
2022-08-25 23:48:13 +0800

    refactor: 重构广告类型

--- src/views/business/ad/type.vue
+++ src/views/business/ad/type.vue
@@ -1,11 +1,9 @@
 <template>
-    <div class="article-classify">
-        <el-form :inline="true">
-            <el-button type="primary" @click="onEdit({})">新建位置</el-button>
-        </el-form>
-
-        <el-table border :data="list" class="mt20">
-            <el-table-column label="广告位名称" prop="title"></el-table-column>
+    <Table :api="getAdPositionList" :pagination="false" ref="tableRef">
+        <template #btns>
+            <el-button type="primary" @click="onEdit({})">新建类型</el-button>
+        </template>
+        <el-table-column label="广告位名称" prop="title"></el-table-column>
             <el-table-column label="广告位描述" prop="description"></el-table-column>
             <el-table-column label="广告位宽度" prop="width"></el-table-column>
             <el-table-column label="广告位高度" prop="height"></el-table-column>
@@ -13,111 +11,79 @@
             <el-table-column label="状态">
                 <template #default="scope">{{$filters.isOpen(scope.row.isOpen)}}</template>
             </el-table-column>
-            <!-- <el-table-column label="分类位置" prop="">
-                <template slot-scope="scope">{{ list[scope.$index].position | getPosition }}</template>
-            </el-table-column> -->
-            <el-table-column label="操作" width="150px;">
-                <template #default="scope">
-                    <el-button type="primary" link @click="onEdit(scope.row)">编辑</el-button>
-                    <el-button type="primary" link @click="onDelete(scope.row.id)">删除</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-        <!-- 分页 -->
-        <!-- <pagination @handleCurrentChange='handleCurrentChange' :total="page.total"></pagination> -->
-        <el-dialog title="新增/编辑" v-model="dialogVisible" width="30%" center>
-            <el-form label-width="120px">
-                <el-form-item label="广告位名称：">
-                    <el-input v-model="form.title"></el-input>
-                </el-form-item>
-                <el-form-item label="广告位描述：">
-                    <el-input v-model="form.description"></el-input>
-                </el-form-item>
-                <el-form-item label="广告位宽度：">
-                    <el-input v-model.number="form.width"></el-input>
-                </el-form-item>
-                <el-form-item label="广告位高度：">
-                    <el-input v-model.number="form.height"></el-input>
-                </el-form-item>
-                <el-form-item label="广告位标识码：">
-                    <el-input v-model="form.mark"></el-input>
-                </el-form-item>
-                <el-form-item label="启用状态：">
-                    <el-checkbox v-model="form.isOpen"></el-checkbox>
-                </el-form-item>
-            </el-form>
-            <span slot="footer" class="dialog-footer">
-                <el-button @click="dialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="onSubmit">保存</el-button>
-            </span>
-        </el-dialog>
-    </div>
+        <el-table-column label="操作" width="150px;">
+            <template #default="scope">
+                <el-button type="primary" link @click="onEdit(scope.row)">编辑</el-button>
+                <el-button type="primary" link @click="onDelete(scope.row.id)">删除</el-button>
+            </template>
+        </el-table-column>
+    </Table>
+
+    <el-dialog :title="form.id ? '编辑' : '新增'" v-model="dialogVisible" width="500px" center>
+        <el-form label-width="120px">
+            <el-form-item label="广告位名称：">
+                <el-input v-model="form.title"></el-input>
+            </el-form-item>
+            <el-form-item label="广告位描述：">
+                <el-input v-model="form.description"></el-input>
+            </el-form-item>
+            <el-form-item label="广告位宽度：">
+                <el-input v-model.number="form.width"></el-input>
+            </el-form-item>
+            <el-form-item label="广告位高度：">
+                <el-input v-model.number="form.height"></el-input>
+            </el-form-item>
+            <el-form-item label="广告位标识码：">
+                <el-input v-model="form.mark"></el-input>
+            </el-form-item>
+            <el-form-item label="启用状态：">
+                <el-checkbox v-model="form.isOpen"></el-checkbox>
+            </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="onSubmit">保存</el-button>
+        </span>
+    </el-dialog>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, onMounted, toRefs } from 'vue'
-import { getAdPositionListApi, delAdPosition, addAdPosition, updateAdPosition } from '@/api/getData'
-import { ElMessageBox } from 'element-plus'
-import { Models } from '@/rapper'
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Table from '@/components/table/index.vue'
+import { getAdPositionList, deleteAdPosition, addAdPosition, updateAdPosition } from '@/api/getData'
+import useMessageBox from '@/hooks/useMessageBox'
 
-type adModel = Models['GET/admin/ad/position/list']['Res']['data']['list'][0]
+let tableRef = ref()
+let form = ref({
+    id: 0,
+    title: '',
+    description: '',
+    width: 0,
+    height: 0,
+    mark: '',
+    isOpen: true
+})
+let dialogVisible = ref(false)
 
-export default defineComponent({
-    setup() {
-        const state = reactive({
-            form: {} as Models['POST/admin/ad/position/update']['Req'],
-            list: [] as adModel[],
-            dialogVisible: false,
-            position: [
-                {label: '系统公告', value: 1}, {label: '使用指南', value: 2}, {label: '常见问题', value: 3}
-            ]
-        })
+const onEdit = (row) => {
+    form.value = { ...row }
+    dialogVisible.value = true
+}
 
-        onMounted(() => {
-            getAdPosition()
-        })
+const onDelete = async(id: number) => {
+    await useMessageBox()
+    await deleteAdPosition({id})
+    tableRef.value.refresh()
+}
 
-        const getAdPosition = async () => {
-            const data = await getAdPositionListApi()
-            state.list = data.list
-        }
+const onSubmit = async () => {
+    if (form.value.id) {
+        await updateAdPosition(form.value)
+    } else {
+        await addAdPosition(form.value)
+    }
+    dialogVisible.value = false
+    tableRef.value.refresh()
+}
 
-        const onDelete = (id: number) => {
-            ElMessageBox.confirm('确认删除此分类吗?', '提示', {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                type: 'warning',
-            })
-            .then(async() => {
-                await delAdPosition({id})
-                getAdPosition()
-            })
-        }
-
-        const onEdit = (row) => {
-            state.form = { ...row }
-            state.dialogVisible = true
-        }
-
-        const onSubmit = async () => {
-            if (!state.form.id) {
-                await addAdPosition(state.form)
-            } else {
-                await updateAdPosition(state.form)
-            }
-            state.dialogVisible = false
-            getAdPosition()
-        }
-
-        return {
-            ...toRefs(state),
-            onDelete,
-            onEdit,
-            onSubmit
-        }
-     }
-  })
 </script>
-
-<style scoped lang="less">
-</style>

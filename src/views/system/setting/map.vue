e9f562f59d1dd9a8aab62b3fbda04f378de25f34
shaoky <634207476@qq.com>
2022-08-25 23:53:58 +0800

    refactor: 重构设置地图

--- src/views/system/setting/map.vue
+++ src/views/system/setting/map.vue
@@ -12,66 +12,49 @@
         <div style="text-align: center;">
             <el-button type="primary" @click="onSave">保存</el-button>
         </div>
-        
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, onMounted, toRefs } from 'vue'
-import { getWebsite, updateWebsite } from '../../../api/getData'
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import { getWebsite, updateWebsite } from '@/api/getData'
 
-export default defineComponent({
-    setup() {
-        const state = reactive({
-            center: {lng: 120.68032, lat: 28.005232},
-            zoom: 13,
-            id: '',
-            website: {
-                baiduMapX: '',
-                baiduMapY: ''
-            },
-            x: null,
-            y: null
-        })
-
-        onMounted(async() => {
-            await getData()
-            var map = new BMapGL.Map("container");
-            // 创建地图实例 
-            var point = new BMapGL.Point(state.website.baiduMapX, state.website.baiduMapY);
-            map.centerAndZoom(point, 14);  
-            map.addControl(new BMapGL.NavigationControl());
-            map.enableDragging();//启用地图拖拽事件，默认启用(可不写)
-            map.enableScrollWheelZoom();//启用地图滚轮放大缩小
-            map.disableDoubleClickZoom(); 
-            map.enableKeyboard();//启用键盘上下左右键移动地图
-            var marker = new BMapGL.Marker(point); 
-            map.addOverlay(marker);
-            map.addEventListener("click", function(e){
-                state.website.baiduMapX = e.latlng.lng
-                state.website.baiduMapY = e.latlng.lat
-                map.removeOverlay(marker);
-                marker = new BMapGL.Marker(new BMapGL.Point(e.latlng.lng,e.latlng.lat));
-                map.addOverlay(marker);
-            });
-        })
-
-        const getData = async () => {
-            const { baiduMapX, baiduMapY } = await getWebsite()
-            state.website.baiduMapX = baiduMapX
-            state.website.baiduMapY = baiduMapY
-        }
-
-        const onSave = async () => {
-            await updateWebsite(state.website)
-        }
-
-        return {
-            ...toRefs(state),
-            onSave
-        }
-    },
+let website = ref({
+    baiduMapX: '',
+    baiduMapY: ''
 })
+
+onMounted(async() => {
+    await getData()
+    let map = new BMapGL.Map("container");
+    // 创建地图实例 
+    let point = new BMapGL.Point(website.value.baiduMapX, website.value.baiduMapY)
+    map.centerAndZoom(point, 14)
+    map.addControl(new BMapGL.NavigationControl())
+    map.enableDragging() //启用地图拖拽事件，默认启用(可不写)
+    map.enableScrollWheelZoom() //启用地图滚轮放大缩小
+    map.disableDoubleClickZoom();
+    map.enableKeyboard();//启用键盘上下左右键移动地图
+    let marker = new BMapGL.Marker(point);
+    map.addOverlay(marker)
+    map.addEventListener("click", function(e){
+        website.value.baiduMapX = e.latlng.lng
+        website.value.baiduMapY = e.latlng.lat
+        map.removeOverlay(marker)
+        marker = new BMapGL.Marker(new BMapGL.Point(e.latlng.lng,e.latlng.lat))
+        map.addOverlay(marker)
+    })
+})
+
+const getData = async () => {
+    const { baiduMapX, baiduMapY } = await getWebsite()
+    website.value.baiduMapX = baiduMapX
+    website.value.baiduMapY = baiduMapY
+}
+
+const onSave = async () => {
+    await updateWebsite(website.value)
+}
 </script>
 
 <style scoped lang="less">

d62a73f3f15962a49c4aa708e3a11dfbef648089
shaoky <634207476@qq.com>
2022-08-25 23:52:58 +0800

    refactor: 重构产品列表

--- src/views/business/goods/index.vue
+++ src/views/business/goods/index.vue
@@ -1,152 +1,95 @@
 <template>
-    <div class="articleIndex">
-        <el-form :inline="true">
-            <el-form-item label="标题：">
-                <el-input v-model="form.title"></el-input>
-            </el-form-item>
-            <el-form-item label="类型：">
-                <!-- <el-select v-model="form.typeId" @change="getGoodsList" placeholder="请选择类型">
-                    <el-option  v-for="item in categoryList" :key="item.value" :label="item.title" :value="item.id"></el-option>
-                </el-select> -->
-                <el-cascader
-                    v-model="goodsClassId"
-                    :options="categoryList"
-                    :props="{ value: 'id', label: 'title' }"
-                    placeholder="请选择"
-                    @change="goodsClassIdChange"
-                    clearable>
-                </el-cascader>
-            </el-form-item>
-            <el-form-item label="状态：" >
-                <el-select v-model="form.isOpen" @change="getGoodsList" placeholder="请选择状态" clearable>
-                    <el-option  v-for="item in status" :key="item.value" :label="item.label" :value="item.value"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="getGoodsList">查询</el-button>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="$router.push({ path: '/business/goods/add' })">新建产品</el-button>
-            </el-form-item>
-        </el-form>
-        <el-table border :data="list">
-            <el-table-column label="标题" prop="title"></el-table-column>
-            <el-table-column label="类型" prop="goodsClassName"></el-table-column>
-            <el-table-column label="图片" prop="imageUrl">
-                <template #default="scope">
-                    <img :src="scope.row.imageUrl" style="max-height: 80px; margin:0 auto; display: block;">
-                </template>
-            </el-table-column>
-            <el-table-column label="价格" prop="price"></el-table-column>
-            <el-table-column label="排序" prop="sort"></el-table-column>
-            <el-table-column label="状态">
-                <template #default="scope">{{$filters.isOpen(scope.row.isOpen)}}</template>
-            </el-table-column>
-            <el-table-column label="操作" width="160px;">
-                <template #default="scope">
-                    <el-button @click="$router.push({name:'goodsInfo',params:{id: scope.row.id}})" type="primary" link>编辑</el-button>
-                    <el-button @click="setIsOpen(scope.row)" type="primary" link>{{scope.row.isOpen ? '下架' : '上架'}}</el-button>
-                    <el-button @click="onDelete(scope.row.id)" type="primary" link>删除</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-         <!-- 分页 -->
-        <pagination @handleCurrentChange='handleCurrentChange' @handleSizeChange="handleSizeChange" :size="form.size" :total="count"></pagination>
-    </div>
+    <Table :api="getGoodsList" :search="searchList" ref="tableRef">
+        <template #btns>
+            <el-button type="primary" @click="$router.push({ path: '/business/article/add' })">新建</el-button>
+        </template>
+        <el-table-column label="标题" prop="title" min-width="200"></el-table-column>
+        <el-table-column label="类型" prop="goodsClassName"></el-table-column>
+        <el-table-column label="图片" prop="imageUrl">
+            <template #default="scope">
+                <img :src="scope.row.imageUrl" class="v-table-image">
+            </template>
+        </el-table-column>
+        <el-table-column label="价格" prop="price" sortable></el-table-column>
+        <el-table-column label="销量" prop="saleCount" sortable></el-table-column>
+        <el-table-column label="库存" prop="stock" sortable></el-table-column>
+        <el-table-column label="排序" prop="sort"></el-table-column>
+        <el-table-column label="状态">
+            <template #default="scope">
+                 <el-switch v-model="scope.row.isOpen" @change="setIsOpen(scope.row)" />
+            </template>
+        </el-table-column>
+        <el-table-column label="操作" width="160px;">
+            <template #default="scope">
+                <el-button @click="$router.push({name: 'goodsInfo', params: {id: scope.row.id}})" type="primary" link>编辑</el-button>
+                <el-button @click="onDelete(scope.row.id)" type="primary" link>删除</el-button>
+            </template>
+        </el-table-column>
+    </Table>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, onMounted, toRefs } from 'vue'
-import { getGoodsListApi, getGoodsTypeListApi, deleteGoods, setGoodsIsOpen } from '../../../api/getData'
-import { ElMessageBox } from 'element-plus'
-import Pagination from '../../../components/pagination.vue'
-import { deleteChildren } from '../../../utils/tools'
-import { Models } from '@/rapper'
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Table from '@/components/table/index.vue'
+import { getGoodsList, getGoodsTypeList, deleteGoods, setGoodsIsOpen } from '@/api/getData'
+import useMessageBox from '@/hooks/useMessageBox'
+import { SearchModel } from '@/components/search/interface'
 
-export default defineComponent({
-    components: {Pagination},
-    setup() {
-        const state = reactive({
-            goodsClassId: undefined,
-            form: {
-                page: 1,
-                size: 10,
-                title: '',
-                goodsClassId: undefined,
-                isOpen: undefined
+let tableRef = ref()
+
+let searchList = ref<SearchModel[]>([
+    {
+        label: '标题',
+        field: 'title',
+        type: 'text',
+        value: '',
+    },
+    {
+        label: '类型',
+        field: 'goodsClassId',
+        type: 'cascader',
+        value: '',
+        api: getGoodsTypeList,
+        params: {
+            type: 2
+        },
+        props: {
+            label: 'title',
+            value: 'id'
+        }
+    },
+    {
+        label: '状态',
+        field: 'isOpen',
+        type: 'select',
+        value: '',
+        items: [
+            {
+                label: '全部',
+                value: '',
             },
-            status: [
-                {label: '全部', value: ''}, {label: '未发布', value: 0}, {label: '已发布', value: 1}
-            ],
-            list: [] as Models['GET/admin/goods/list']['Res']['data']['list'],
-            count: 0,
-            categoryList: [] as Models['GET/admin/goods/type/list']['Res']['data']['list']
-        })
+            {
+                label: '未发布',
+                value: 0,
+            },
+            {
+                label: '已发布',
+                value: 1,
+            },
+        ]
+    }
+])
 
-        onMounted(() => {
-            getGoodsList()
-            getGoodsTypeList()
-        })
+const onDelete = async(id: number) => {
+    await useMessageBox()
+    await deleteGoods({id})
+    tableRef.value.refresh()
+}
 
-        const getGoodsList = async () => {
-            const data = await getGoodsListApi(state.form)
-            state.list = data.list
-            state.count = data.count
-        }
-
-        const getGoodsTypeList = async () => {
-            const data = await getGoodsTypeListApi({type: 2})
-            deleteChildren(data.list)
-            state.categoryList = data.list
-        }
-
-        const onDelete = (id: number) => {
-            ElMessageBox.confirm('确认删除吗?', '提示', {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                type: 'warning',
-            })
-            .then(async() => {
-                await deleteGoods({id: id})
-                getGoodsList()
-            })
-        }
-
-        const handleCurrentChange = (page: number) => {
-            state.form.page = page
-            getGoodsList()
-        }
-
-        const handleSizeChange = (value: number) => {
-            state.form.size = value
-            getGoodsList()
-        }
-
-        const goodsClassIdChange = (data) => {
-            state.form.goodsClassId = data[data.length-1]
-            getGoodsList()
-        }
-
-        const setIsOpen = async(data) => {
-            await setGoodsIsOpen({
-                id: data.id,
-                isOpen: data.isOpen ? 0 : 1
-            })
-            data.isOpen = !data.isOpen
-        }
-
-        return {
-            ...toRefs(state),
-            getGoodsList,
-            onDelete,
-            goodsClassIdChange,
-            handleCurrentChange,
-            handleSizeChange,
-            setIsOpen
-        }
-     }
-  })
+const setIsOpen = async(data) => {
+    await setGoodsIsOpen({
+        id: data.id,
+        isOpen: data.isOpen ? 1 : 0
+    })
+}
 </script>
-
-<style scoped lang="less">
-</style>
